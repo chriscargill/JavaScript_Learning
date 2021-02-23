2d60ee9609364837a829003da76cd48cea0c3488
chriscargill <chriscargill07@gmail.com>
2021-02-22 20:24:12 -0500

    Added more functionality to snake game
    Game loop Added
    Out of bounds Added
    
    Changes to be committed:
    	modified:   main.js
    	modified:   snake.html

--- snake_game/main.js
+++ snake_game/main.js
@@ -1,12 +1,13 @@
 let last_render_time = 0
-const SNAKE_SPEED =  3
-const snake_body = [ { x: 11, y: 11 }]
+const SNAKE_SPEED =  10
+let snake_body = [ { x: 11, y: 11 }]
 const game_board = document.getElementById('game-board')
-var input_direction = { x: 0 , y: 0 }
+let input_direction = { x: 0 , y: 0 }
 let last_direction = { x: 0, y: 0 }
 
-let food = [{ x: 2 , y: 2 }]
+let food = [{ x: getRandomNumber() , y: getRandomNumber() }]
 let new_segments = 0
+let isPlaying = true
 
 window.addEventListener('keydown', e => {
     switch (e.key) {
@@ -34,11 +35,15 @@ function main(current_time){
     const seconds_since_last_render = (current_time - last_render_time) / 1000
     if (seconds_since_last_render < 1 / SNAKE_SPEED) return
     
-    console.log("Render")
     last_render_time = current_time
-    update()
-    draw(game_board)
-    draw_food(game_board)
+    if (isPlaying === true){
+        update()
+        draw(game_board)
+        draw_food(game_board)
+    } else {
+        showRestartScreen()
+    }
+
 
 }
 
@@ -48,12 +53,13 @@ function getInputDirection(){
 }
 function update(){
     updateSnake()
+    checkBounds()
     updateFood()
     addSegments()
 }
 
 function draw(gameBoard){
-    // console.log("draw snake")
+
     gameBoard.innerHTML = ''
     snake_body.forEach(segment => {
         const snake_element = document.createElement('div')
@@ -65,7 +71,7 @@ function draw(gameBoard){
 }
 
 function draw_food(gameBoard){
-    // console.log("draw food")
+
     food.forEach(segment => {
         const food_item = document.createElement('div')
         food_item.style.gridRowStart = segment.y
@@ -77,13 +83,14 @@ function draw_food(gameBoard){
 
 function updateSnake(){
     input_direction = getInputDirection()
-    // console.log("update")
+
     for (let i = snake_body.length - 2; i >= 0; i--) {
         snake_body[ i + 1 ] = { ...snake_body[i] }
     }
     snake_body[0].x += input_direction.x
     snake_body[0].y += input_direction.y
 }
+
 function updateFood(){
     food.forEach( piece => {
         if (onSnake(piece)) {
@@ -103,13 +110,39 @@ function onSnake(location){
     return value
 }
 
+function onSnakeHeadBody(){
+    let value = false
+    snake_body.slice(1).forEach( loc => {
+        if (snake_body[0].x === loc.x && snake_body[0].y === loc.y){
+            value = true
+        }
+
+    })
+    return value
+}
+
+function onSnakeHeadFood(one,two){
+    let value = false
+    if (snake_body[0].x === one && snake_body[0].y === two){
+        value = true
+    }
+    return value
+}
+
 function expandSnake(amount){
     new_segments += amount
 }
 
 function removeFood(){
-    console.log("removing food")
-    food = [{x: 20, y: 20}]
+    let one = getRandomNumber()
+    let two = getRandomNumber()
+
+    while (onSnakeHeadFood(one,two)){
+        one = getRandomNumber()
+        two = getRandomNumber()
+    }
+
+    food = [{x: one, y: two}]
 }
 
 function addSegments(){
@@ -119,5 +152,45 @@ function addSegments(){
         }
         new_segments = 0
     }
+}
+
+function getRandomNumber(){
+    const mr = Math.random()
+    const rando = Math.floor(mr * 100)
+    if (rando > 21) { // Bring it back into range of the grid
+        const new_rando = Math.floor(rando/5)
+        return new_rando
+    } else {
+        return rando
+    }
+}
+
+function checkBounds(){
+    const head = snake_body.slice(0,1)[0]
+    if (head.x < 1 || head.x > 21){
+        alert("FAILED")
+        isPlaying = false
+    }
+    if (head.y < 1 || head.y > 21){
+        alert("FAILED")
+        isPlaying = false
+    }
+}
+function showRestartScreen(){
+    game_board.innerHTML = ''
+    let div = document.createElement('div') 
+    let button_div = document.createElement('button')
+    button_div.classList.add('restart')
+    button_div.addEventListener('click', e => {
+        console.log("CLICKED")
+        e.preventDefault()
+        snake_body = [{ x: 11, y: 11 }]
+        input_direction = { x: 0 , y: 0 }
+        last_direction = { x: 0, y: 0 }
+        food = [{ x: getRandomNumber() , y: getRandomNumber() }]
+        isPlaying = true
+    })
+    div.appendChild(button_div)
+    game_board.appendChild(div)
 }
 window.requestAnimationFrame(main)
